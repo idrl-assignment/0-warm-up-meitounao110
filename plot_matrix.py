f1855c46a43c91835da80cdc6286dd0d1e9d71df
zyy <431041317@qq.com>
2020-11-26 15:24:08 +0800

    test

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,13 +2,16 @@
 import numpy as np
 import imageio
 
+
 def generate_random_matrix(m, n):
     matrix = np.random.randint(0, 2, (m, n))
     #matrix = np.mat(matrix)
     return matrix
 
+
 def save_matrix(matrix, file_name):
     imageio.imwrite(file_name, matrix)
+
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
